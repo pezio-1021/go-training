f57cb53b778b257b10dcff5d1b29cfa1bf80ab28
pezio <iloveoneokbaske@gmail.com>
2021-04-29 00:28:34 +0900

    page 60

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	var h http.Handler = http.HandlerFunc(home)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = h
+	http.ListenAndServe(":3000", r)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>We could not find the page you "+
+		"were looking for :(</h1>"+
+		"<p>Please email us if you keep being sent to an "+
+		"invalid page. </p>")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ</h1>")
+}
