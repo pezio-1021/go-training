2d4059e4f8e701a62023f98923150e5cfdc29bf1
pezio <iloveoneokbaske@gmail.com>
2021-05-05 01:47:42 +0900

    P:225

--- exp/main.go
+++ exp/main.go
@@ -1,34 +1,62 @@
 package main
 
 import (
-	"database/sql"
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/lib/pq"
 )
 
 const (
-	host = "localhost"
-	port = "5433"
-	user = "admin"
+	host     = "localhost"
+	port     = 5433
+	user     = "admin"
 	password = "admin"
-	dbname = "lenslocked_dev"
+	dbname   = "lenslocked_dev"
 )
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+}
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-	"password=%s dbname=%s sslmode=disable",
-	host, port, user, password, dbname)
-	
-	db, err := sq;.Open("postgress", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	
-	err = db.Ping()
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Successfully connected!")
-	db.Close()
+	defer db.Close()
+	db.LogMode(true)
+	db.AutoMigrate(&User{})
+
+	name, email := getInfo()
+	u := &User{
+		Name:  name,
+		Email: email,
+	}
+	if err := db.Create(u).Error; err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", u)
+}
+
+func getInfo() (name, email string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	name, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	fmt.Println("What is your email?")
+	email, _ = reader.ReadString('\n')
+	email = strings.TrimSpace(email)
+	return name, email
 }
